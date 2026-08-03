3e89957641560d1bb860125d4646bbb3a2d6641b
Thehxcoach <bairdstaciel@gmail.com>
2026-08-03 16:45:09 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+import HRPage from './HRPage.jsx'
+
+function Root() {
+  const [hash, setHash] = useState(window.location.hash)
+  useEffect(() => {
+    const onHash = () => setHash(window.location.hash)
+    window.addEventListener('hashchange', onHash)
+    return () => window.removeEventListener('hashchange', onHash)
+  }, [])
+  return hash.startsWith('#/hr') ? <HRPage /> : <App />
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
