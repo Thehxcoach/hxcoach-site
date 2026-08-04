d65fcff61d1fd99f3e3f5fc34f6daaf040f17485
Thehxcoach <bairdstaciel@gmail.com>
2026-08-03 17:16:41 -0700

    add hr page routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-
+import './index.css'
 import App from './App.jsx'
 import HRPage from './HRPage.jsx'
 
